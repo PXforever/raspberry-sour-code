fa148e539b7009ae3d0eab794628b99b0766f827
pengxuan01 <332083570@qq.com>
2018-08-19 03:06:14 -0700

    Init source code

---
+++ raspberry.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+PWD=/home/pengxuan01/tools-master/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin 
+export PATH="$PATH:$PWD/"
+export ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- KERNEL=kernel7
